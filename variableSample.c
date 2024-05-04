b3f842ab87557f8eb2657404348175963ed0252c
Kirishan-Thavagnanaranjan <Thavagnanaranjankirishan@gmail.com>
2024-05-05 00:11:39 +0530

    Add variableSample.c file

---
+++ variableSample.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int employeeNumber=23110;
+    char employeeGrade='C';
+    float salary=55000.00;
+
+    printf("Employee Number: %d %n", employeeNumber);
+    printf("Employee Grade: %c %n" ,employeeGrade );
+    printf("Employee salary: %f %n" , salary);
+    return 0 ;    
+}
